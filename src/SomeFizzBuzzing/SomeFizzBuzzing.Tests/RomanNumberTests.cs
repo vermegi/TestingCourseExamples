285200d1c2fc36f2e2d9c0d651546c6530a50f90
vermegi <gitte.vermeiren@gmail.com>
2014-11-18 13:16:44 +0100

    tdd romannumbers

---
+++ src/SomeFizzBuzzing/SomeFizzBuzzing.Tests/RomanNumberTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+
+namespace SomeFizzBuzzing.Tests
+{
+    [TestFixture]
+    public class RomanNumberTests
+    {
+        [Test]
+        public void It_returns_I_for_1()
+        {
+            var sut = new RomanNumberGenerator();
+
+            var result = sut.GenerateFor(1);
+
+            Assert.That(result, Is.EqualTo("I"));
+        }
+
+        [Test]
+        public void It_returns_II_for_2()
+        {
+            var sut = new RomanNumberGenerator();
+
+            var result = sut.GenerateFor(2);
+
+            Assert.That(result, Is.EqualTo("II"));
+        }
+    }
+}
---
+++ src/SomeFizzBuzzing/SomeFizzBuzzing/RomanNumberGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+namespace SomeFizzBuzzing
+{
+    public class RomanNumberGenerator
+    {
+        public string GenerateFor(int getal)
+        {
+            var sb = new StringBuilder();
+            for (int i = 0; i < getal; i++)
+            {
+                sb.Append("I");
+            }
+            return sb.ToString();
+        }
+    }
+}
